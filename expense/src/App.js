cd11d383e9c768ff1fcc0dbfd9ea607b7c1cb6f5
Sujeong Kim <62699938+sujeong816@users.noreply.github.com>
2023-02-01 21:41:43 +0900

    Update App.js

--- expense/src/App.js
+++ expense/src/App.js
@@ -1,11 +1,31 @@
-import "./index.css";
-import ExpenseItem from "./component/ExpenseItem";
+import Expenses from "./component/Expenses";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94000,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", 
+      title: "New TV", 
+      amount: 799000, 
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294000,
+      date: new Date(2021, 2, 28),
+    },
+    
+  ];
+
   return (
     <div>
-      Hello World!
-      <ExpenseItem />
+      <h2>Hello World!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }

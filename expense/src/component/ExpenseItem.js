f4dfaafb6fa3fea5f5266fc0ab75521d0a72a59d
Sujeong Kim <62699938+sujeong816@users.noreply.github.com>
2023-02-01 21:42:23 +0900

    Update ExpenseItem.js

--- expense/src/component/ExpenseItem.js
+++ expense/src/component/ExpenseItem.js
@@ -1,11 +1,14 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
-export default function ExpenseItem() {
+export default function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div className="expense-item__description">Date</div>
-      <h2>Title</h2>
-      <div className="expense-item__price">Amount</div>
+      <ExpenseDate date={props.date}/> {/** ExpenseDate의 props를 통해 date를 받아옴*/}
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}원</div>
+      </div>
     </div>
   );
 }

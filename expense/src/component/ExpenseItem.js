1248e08efe0953bf4e687b56a27754b22162d48c
Sujeong Kim <62699938+sujeong816@users.noreply.github.com>
2023-02-03 02:23:15 +0900

    Update ExpenseItem.js
    Card 컴포넌트 추가

--- expense/src/component/ExpenseItem.js
+++ expense/src/component/ExpenseItem.js
@@ -1,14 +1,15 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "./UI/Card";
 
 export default function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date}/> {/** ExpenseDate의 props를 통해 date를 받아옴*/}
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}원</div>
       </div>
-    </div>
+    </Card>
   );
 }

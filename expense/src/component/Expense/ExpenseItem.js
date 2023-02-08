d918936bd6881db10f231fd828797faf66fec5f6
sujeong816 <sujeong1517@naver.com>
2023-02-08 22:49:42 +0900

    commit log

--- expense/src/component/Expense/ExpenseItem.js
+++ expense/src/component/Expense/ExpenseItem.js
@@ -1,15 +1,26 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+import { useState } from "react";
 
 export default function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title); /** [ , ]: 독립적인 두 요소인 상수나 변수를 저장하기 위함
+   title: 관리되고 있는 값. 현재 설정된 값. 초기값으로 props.title 저장
+   setTitle: 새로운 title을 설정하기 위해 호출 가능한 함수. 현재 설정된 값 업데이트 위한 함수*/
+
+  function clickedHandler() {
+    setTitle('Update!');
+    console.log('Click!');
+  }
+
   return (
-    <Card className="expense-item">
+    <Card className='expense-item'>
       <ExpenseDate date={props.date}/> {/** ExpenseDate의 props를 통해 date를 받아옴*/}
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.amount}원</div>
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>{props.amount}원</div>
       </div>
+      <button onClick={clickedHandler}>Changed Title</button>
     </Card>
   );
 }
--- expense/src/component/Expense/Expenses.js
+++ expense/src/component/Expense/Expenses.js
@@ -1,29 +1,42 @@
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpenseFilter";
+import { useState } from "react";
 
 export default function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2023");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
 
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
 
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
+        <ExpenseItem
+          title={props.items[0].title}
+          amount={props.items[0].amount}
+          date={props.items[0].date}
+        />
 
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
+        <ExpenseItem
+          title={props.items[1].title}
+          amount={props.items[1].amount}
+          date={props.items[1].date}
+        />
 
-    </Card>
+        <ExpenseItem
+          title={props.items[2].title}
+          amount={props.items[2].amount}
+          date={props.items[2].date}
+        />
+      </Card>
+    </div>
   );
 }

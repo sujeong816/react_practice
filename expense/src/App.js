d53b5ef148cc8868c8982b60975428c1a218eb4a
sujeong816 <sujeong1517@naver.com>
2023-03-19 02:03:43 +0900

    날짜 수정

--- expense/src/App.js
+++ expense/src/App.js
@@ -1,31 +1,32 @@
-import Expenses from './component/Expense/Expenses';
-import NewExpense from './component/NewExpense/NewExpense';
+import Expenses from "./component/Expense/Expenses";
+import NewExpense from "./component/NewExpense/NewExpense";
 
-const App = () => { /** syntax 또 다른 함수 작성 방법 */
+const App = () => {
+  /** syntax 또 다른 함수 작성 방법 */
   const expenses = [
     {
-      id: 'e1',
-      title: 'Toilet Paper',
+      id: "e1",
+      title: "Toilet Paper",
       amount: 94.12,
       date: new Date(2023, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2023, 2, 12) },
     {
-      id: 'e3',
-      title: 'Car Insurance',
+      id: "e3",
+      title: "Car Insurance",
       amount: 294.67,
       date: new Date(2024, 2, 28),
     },
     {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
+      id: "e4",
+      title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2024, 5, 12),
     },
   ];
 
   function addExpenseHandler(expense) {
-    console.log('In App.js');
+    console.log("In App.js");
     console.log(expense);
   }
 
@@ -35,6 +36,6 @@ const App = () => { /** syntax 또 다른 함수 작성 방법 */
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;

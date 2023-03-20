9d156d9d2650a9bcb78e90ef3abf31940e39337c
sujeong816 <sujeong1517@naver.com>
2023-03-20 12:36:15 +0900

    컴포넌트 입력 중 취소할 수 있는 Cancel 버튼 구현

--- expense/src/component/NewExpense/NewExpense.js
+++ expense/src/component/NewExpense/NewExpense.js
@@ -1,18 +1,36 @@
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
+import { useState } from "react";
 
 export default function NewExpense(props) {
+  const [isEditing, setIsEditing] = useState(false);
+
   function SaveExpenseDate(enteredExpenseData) {
-    const expenseData = { /** ExpenseForm의 submitHandler */
-        ...enteredExpenseData,
-        id: Math.random().toString() 
+    const expenseData = {
+      /** ExpenseForm의 submitHandler */ 
+      ...enteredExpenseData,
+      id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
   }
 
+  function startEditingHandler() {
+    setIsEditing(true);
+  }
+
+  function stopEditingHandler() {
+    setIsEditing(false);
+  }
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={SaveExpenseDate} />
+      {!isEditing && <button onClick={startEditingHandler}>추가</button>}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={SaveExpenseDate}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 }

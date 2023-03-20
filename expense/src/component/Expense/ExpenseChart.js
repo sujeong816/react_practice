ec3feb11aa84996f3847526c93fb241211fc9deb
sujeong816 <sujeong1517@naver.com>
2023-03-20 22:55:36 +0900

    차트 컴포넌트 추가

---
+++ expense/src/component/Expense/ExpenseChart.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Chart from "../Chart/Chart";
+
+export default function ExpenseChart(props) {
+    const chartDataPoints = [
+        { label: '1월', value: 0 },
+        { label: '2월', value: 0 },
+        { label: '3월', value: 0 },
+        { label: '4월', value: 0 },
+        { label: '5월', value: 0 },
+        { label: '6월', value: 0 },
+        { label: '7월', value: 0 },
+        { label: '8월', value: 0 },
+        { label: '9월', value: 0 },
+        { label: '10월', value: 0 },
+        { label: '11월', value: 0 },
+        { label: '12월', value: 0 }
+    ]
+    
+    for(const expense of props.expenses) {
+        const expenseMonth = expense.date.getMonth(); // 0 => 1월 => 0
+        chartDataPoints[expenseMonth].value += expense.amount;
+    }
+
+    return <Chart dataPoints={chartDataPoints} />
+}
--- expense/src/component/Expense/Expenses.js
+++ expense/src/component/Expense/Expenses.js
@@ -3,6 +3,7 @@ import "./Expenses.css";
 import ExpensesFilter from "./ExpenseFilter";
 import { useState } from "react";
 import ExpensesList from "./ExpensesList";
+import ExpenseChart from "./ExpenseChart";
 
 export default function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2023");
@@ -22,6 +23,7 @@ export default function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        <ExpenseChart expenses={filteredExpenses}/>
         <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>

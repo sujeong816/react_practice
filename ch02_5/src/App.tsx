6480597c74a719132db3833b1d060668cb3f4df8
sujeong816 <sujeong1517@naver.com>
2023-04-17 12:11:43 +0900

    오류 수정

--- ch02_5/src/App.tsx
+++ ch02_5/src/App.tsx
@@ -1,6 +1,22 @@
-import React from 'react';
-import Counter from './Counter';
+import React, { useState } from "react";
+import Counter from "./Counter";
+import Message from "./Message";
+import { IncreaseFunc, SetMessageFunc, State } from "./types";
 
-export default function App(){
-  return <Counter />
+export default function App() {
+  const initialState: State = { message: "hello", count: 0 };
+  const [state, setState] = useState<State>(initialState);
+  const increase: IncreaseFunc = () =>
+    setState({ ...state, count: state.count + 1 });
+  const setMessage: SetMessageFunc = (msg: string) =>
+    setState({ ...state, message: msg });
+
+  return (
+    <div style={{textAlign:"center"}}>
+      <p>message: {state.message}</p>
+      <Message message={state.message} setMessage={setMessage}/>
+      <hr/>
+      <Counter count={state.count} increase={increase}/>
+    </div>
+    );
 }

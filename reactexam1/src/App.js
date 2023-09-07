223d42581697ec1a856cf968e451f03943962316
sujeong816 <sujeong1517@naver.com>
2023-09-07 22:36:17 +0900

    style 삭제, Counter 컴포넌트 추가

--- reactexam1/src/App.js
+++ reactexam1/src/App.js
@@ -1,32 +1,12 @@
-// import "./App.css";
-import MyFooter from "./MyFooter";
+import Counter from "./Counter";
 import MyHeader from "./MyHeader";
 
 function App() {
-  const name = "김수정";
-
-  const style = {
-    App: {
-      backgroundColor: "black",
-    },
-    h2: {
-      color: "red",
-    },
-    bold_text: {
-      color: "green"
-    }
-
-  }
-  const number = 5
   
   return (
-    <div style={style.App}>
+    <div>
       <MyHeader />
-      <h2 style={style.h2}>안녕 리액트 {name}</h2>
-      <b style={style.bold_text} id="bold_text">
-        {number}는 {number%2===0 ? "짝수" : "홀수"}이다.
-      </b>
-      <MyFooter />
+      <Counter/>
     </div>
   );
 }

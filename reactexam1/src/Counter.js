ddbce0cc4025bd958403076e19615636840e95c6
Sujeong Kim <lewis14081626@gmail.com>
2023-09-08 23:24:41 +0900

    props 추가

--- reactexam1/src/Counter.js
+++ reactexam1/src/Counter.js
@@ -1,11 +1,28 @@
-function Counter() {
+import { useState } from "react"
+import React from "react"
+import OddEvenResult from "./OddEvenResult"
+
+function Counter({initialValue}) {
+
+    const [count, setCount] = useState(initialValue) //0부터 시작
+
+    function onIncrease() {
+        setCount(count + 1)
+    }
+
+    function onDecrease() {
+        setCount(count - 1)
+    }
+
     return (
         <div>
-            <h2>0</h2>
-            <button>+</button>
-            <button>-</button>
+            <h2>{count}</h2>
+            <button onClick={onIncrease}>+</button>
+            <button onClick={onDecrease}>-</button>
+            <OddEvenResult count={count} />
         </div>
     )
 }
 
+Counter.defaultProps = { initialValue: 0 } //인자가 넘어오지 않을 때 생기는 오류를 대비
 export default Counter
